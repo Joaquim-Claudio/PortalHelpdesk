a4d5879bf7ad0b996f3c55d38c181a2b20eb43b7
Joaquim Claudio <joaquim.claudio2003@gmail.com>
2025-09-23 11:59:10 +0100

    Health Check Controller

---
+++ src/PortalHelpdesk/Controllers/HealthCheckController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortalHelpdesk.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HealthCheckController: ControllerBase
+    {
+        private readonly ILogger<HealthCheckController> _logger;
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Health check OK");
+            return Ok("API is running");
+        }
+    }
+}
